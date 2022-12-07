7bc2e38a50d5f985920927f3427a388fc194cbe6
Adi Munawar <munawar.adi31@gmail.com>
2022-12-07 21:47:12 +0700

    add method post in books

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello GO")
+	router := gin.Default()
+
+	router.GET("/", rootHandler)
+	router.GET("/blog", blogHandler)
+	router.POST("/books", postBooksHandler)
+	router.GET("/query", queryHandler)
+	router.GET("/:id", dynamicHandler)
+
+	router.Run(":3000")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Adi Munawar",
+		"age":  31,
+	})
+}
+
+func dynamicHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	c.JSON(http.StatusOK, gin.H{"title": title})
+}
+
+func blogHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":       "Belajar Pemrograman Golang Untuk pemula",
+		"description": "Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta. Nulla porttitor accumsan tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Pellentesque in ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget malesuada. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Proin eget tortor risus.",
+	})
+}
+
+type BookInput struct {
+	Title    string
+	Price    int
+	Subtitle string `json:"sub_title"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":     bookInput.Title,
+		"sub_title": bookInput.Subtitle,
+		"price":     bookInput.Price,
+	})
 }
