db208357fb1233fd5d2bbd727856cd737e8b0546
Adi Munawar <munawar.adi31@gmail.com>
2022-12-08 21:06:16 +0700

    add a book module

--- handler/books.go
+++ handler/books.go
@@ -2,8 +2,12 @@ package handler
 
 import (
 	"net/http"
+	"pustaka-api/book"
+
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func RootHandler(c *gin.Context) {
@@ -27,5 +31,31 @@ func BlogHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":       "Belajar Pemrograman Golang Untuk pemula",
 		"description": "Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta. Nulla porttitor accumsan tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Pellentesque in ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget malesuada. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Proin eget tortor risus.",
+	})
+}
+
+func PostBooksHandler(c *gin.Context) {
+	var bookInput book.BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+
+		errorMessages := []string{}
+
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			// "status":  "error",
+			"message": errorMessages,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
 	})
 }
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -19,38 +15,7 @@ func main() {
 	v1.GET("/blog", handler.BlogHandler)
 	v1.GET("/query", handler.QueryHandler)
 	v1.GET("/:id", handler.DynamicHandler)
-	v1.POST("/books", postBooksHandler)
+	v1.POST("/books", handler.PostBooksHandler)
 
 	router.Run(":3000")
-}
-
-type BookInput struct {
-	Title string      `json:"title" binding:"required"`
-	Price json.Number `json:"price" binding:"required,number"`
-}
-
-func postBooksHandler(c *gin.Context) {
-	var bookInput BookInput
-
-	err := c.ShouldBindJSON(&bookInput)
-	if err != nil {
-
-		errorMessages := []string{}
-
-		for _, e := range err.(validator.ValidationErrors) {
-			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
-			errorMessages = append(errorMessages, errorMessage)
-		}
-
-		c.JSON(http.StatusBadRequest, gin.H{
-			// "status":  "error",
-			"message": errorMessages,
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"title": bookInput.Title,
-		"price": bookInput.Price,
-	})
 }
