7817a020d84d1c4d61f69e4b4fce1be7befa6087
Adi Munawar <munawar.adi31@gmail.com>
2022-12-12 10:49:59 +0700

    auto migration table books

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "host=localhost user=postgres password=31072001 dbname=pustaka-api port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("DB connection Error!")
+	}
+
+	fmt.Println("Postgree SQL connected successfully")
+
+	db.AutoMigrate(&book.Book{})
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
